fca2653045e26a6e8d93751f4edf057de4003cfa
Abhishek <abhibhardwaj1017@gmail.com>
2022-07-02 22:32:04 +0530

    working on blogs

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,18 +4,18 @@ import "./projects.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import ProjectCard from './ProjectCard';
-import { client, urlFor } from "../../client"
+// import { client, urlFor } from "../../client"
 
 
 const Porjects = () => {
-  const [projectData, setProjectData] = useState([])
+  // const [projectData, setProjectData] = useState([])
   useEffect(() => {
     window.scrollTo(0, 0)
     document.title = "Abhishek-Projects"
-    const query = '*[_type=="projects"]'
-    client.fetch(query)
-      .then((data) => setProjectData(data))
-    console.log(projectData)
+    // const query = '*[_type=="projects"]'
+    // client.fetch(query)
+    //   .then((data) => setProjectData(data))
+    // console.log(projectData)
   }, [])
   return (
     <Container fluid className="pt-5 mt-5 projects-container">
@@ -24,6 +24,12 @@ const Porjects = () => {
       </h1>
 
       <Container className="projects-cards-container py-3 animated-right">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
         <ProjectCard />
       </Container>
 
