b03ed10c047e6254c10325e2b365b8c69de597c9
Abhishek <abhibhardwaj1017@gmail.com>
2022-07-02 21:18:06 +0530

    sanity configured

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,16 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from "react-bootstrap";
 import "./projects.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import ProjectCard from './ProjectCard';
+import { client, urlFor } from "../../client"
 
 
 const Porjects = () => {
+  const [projectData, setProjectData] = useState([])
   useEffect(() => {
     window.scrollTo(0, 0)
     document.title = "Abhishek-Projects"
-  })
+    const query = '*[_type=="projects"]'
+    client.fetch(query)
+      .then((data) => setProjectData(data))
+    console.log(projectData)
+  }, [])
   return (
     <Container fluid className="pt-5 mt-5 projects-container">
       <h1 className="heading text-center text-white animated-left">
