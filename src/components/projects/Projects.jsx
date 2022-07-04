3571c25e34d03dc261306323f7af2e61117f5cf6
Abhishek <abhibhardwaj1017@gmail.com>
2022-07-04 20:31:49 +0530

    promise converted to async

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,29 +7,36 @@ import ProjectCard from './ProjectCard';
 import { client } from "../../client"
 import Spinner from "../spinner/Spinner"
 
-
 const Porjects = () => {
   const [projectData, setProjectData] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
-    setLoading(true)
     window.scrollTo(0, 0)
     document.title = "Abhishek-Projects"
-    const query = '*[_type=="projects"]'
-    client.fetch(query)
-      .then((data) => setProjectData(data))
-    setLoading(false)
+    const fetchProjectData = async () => {
+      try {
+        const query = '*[_type=="projects"]'
+        const fetchedProjectData = await client.fetch(query)
+        setProjectData(fetchedProjectData)
+      }
+      catch (err) {
+        console.log(err)
+      }
+      setLoading(false)
+    }
+    fetchProjectData();
   }, [])
   return (
     <Container fluid className="py-5 mt-5 projects-container">
       <h1 className="heading text-center text-white animated-left">
         My <span className="text-primary">Projects</span> <span className="text-danger"><FontAwesomeIcon icon={faBriefcase} /></span>
       </h1>
-      {
-        loading && <Spinner />
-      }
 
       <Container className="projects-cards-container py-3 ">
+        {
+          loading && <Spinner />
+        }
+
         {
           projectData.map((elem, index) => {
             const { project_name, project_dec, project_images, project_url, project_stack } = elem
