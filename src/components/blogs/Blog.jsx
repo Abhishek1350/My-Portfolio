12ef97d7cbd806f35ba234b1848d3ec379c71eac
Abhishek <abhibhardwaj1017@gmail.com>
2022-07-05 20:42:52 +0530

    working on blogs

---
+++ src/components/blogs/Blog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div>Working On It</div>
+  )
+}
+
+export default Blog
--- src/components/blogs/BlogCard.jsx
+++ src/components/blogs/BlogCard.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 
 const BlogsCard = () => {
     return (
-        <Link to={'/blog-card/metaverse'} className="text-white blog-card-box-link">
+        <Link to={'/blogs/:id'} className="text-white blog-card-box-link">
             <motion.div
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 0.6, type: "tween" }}
                 whileHover={{scale:1.04}}
                 className="blog-card-box">
                 <div className="blog-card-img">
-                    <img src={profilePic} alt="metaverse" />
+                    <img src={profilePic} alt="blog-img" />
                 </div>
                 <div className="blog-card-dec my-2">
                     <h4 className="paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing ?</h4>
