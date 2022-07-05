3475847dc0b3aca5a3922a0f804e5a403cb82767
Abhishek <abhibhardwaj1017@gmail.com>
2022-07-05 20:50:48 +0530

    blog

--- src/components/blogs/Blog.jsx
+++ src/components/blogs/Blog.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserNinja } from "@fortawesome/free-solid-svg-icons";
 import { Container } from "react-bootstrap";
 
