9f40a2ab118d2f96a7f565d7ed6274698f1c524f
Abhishek <abhibhardwaj1017@gmail.com>
2022-07-05 20:48:38 +0530

    blog page

--- src/components/blogs/Blog.jsx
+++ src/components/blogs/Blog.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { faUserNinja } from "@fortawesome/free-solid-svg-icons";
+import { Container } from "react-bootstrap";
 
 const Blog = () => {
   return (
-    <div>Working On It</div>
+    <Container fluid className="pt-5 mt-5 blogs-container">
+      <h1 className="heading text-center text-white  animated-left">
+        Working On It <span className="text-danger"><FontAwesomeIcon icon={faUserNinja} /></span>
+      </h1>
+
+    </Container>
   )
 }
 
