f892fb5631d64216a5d5db0ff0f073f1a4df0f75
Abhishek <abhibhardwaj1017@gmail.com>
2024-02-03 19:34:46 +0530

    Feat : Themes and packages

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,11 +2,14 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react'
+import {ThemeProvider as NextThemesProvider} from "next-themes";
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      {children}
+      <NextThemesProvider attribute="class" defaultTheme="system">
+        {children}
+      </NextThemesProvider>
     </NextUIProvider>
   )
 }
---
+++ components/theme-toggler.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Button } from "@nextui-org/react";
+
+export function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  return (
+    <div>
+      The current theme is: {theme}
+      <Button onClick={() => setTheme('light')}>Light Mode</Button>
+      <Button onClick={() => setTheme('dark')}>Dark Mode</Button>
+    </div>
+  )
+};
