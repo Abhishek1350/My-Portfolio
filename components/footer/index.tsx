af87898bb1da89b3b0a4fb89a616bc15c57eeca9
Abhishek <abhibhardwaj1017@gmail.com>
2024-02-23 23:10:50 +0530

    Change : Footer design

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,118 +1,81 @@
-"use client";
-
+import { Container, Group, ActionIcon, rem, Title, Text } from "@mantine/core";
 import {
-  Text,
-  Container,
-  ActionIcon,
-  Group,
-  rem,
-  Image,
-  Box,
-} from "@mantine/core";
-import {
+  IconBrandGithub,
   IconBrandTwitter,
-  IconBrandYoutube,
   IconBrandInstagram,
 } from "@tabler/icons-react";
 import classes from "./styles.module.css";
-import { MotionDiv } from "..";
-
-const data = [
-  {
-    title: "About",
-    links: [
-      { label: "Features", link: "#" },
-      { label: "Pricing", link: "#" },
-      { label: "Support", link: "#" },
-      { label: "Forums", link: "#" },
-    ],
-  },
-  {
-    title: "Project",
-    links: [
-      { label: "Contribute", link: "#" },
-      { label: "Media assets", link: "#" },
-      { label: "Changelog", link: "#" },
-      { label: "Releases", link: "#" },
-    ],
-  },
-  {
-    title: "Community",
-    links: [
-      { label: "Join Discord", link: "#" },
-      { label: "Follow on Twitter", link: "#" },
-      { label: "Email newsletter", link: "#" },
-      { label: "GitHub discussions", link: "#" },
-    ],
-  },
-];
+import Link from "next/link";
+import { TypingEffect } from "./Typing-Effect";
 
 export function Footer() {
-  const groups = data.map((group) => {
-    const links = group.links.map((link, index) => (
-      <Text<"a">
-        key={index}
-        className={classes.link}
-        component="a"
-        href={link.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </Text>
-    ));
-
-    return (
-      <Box className={classes.wrapper} key={group.title}>
-        <Text className={classes.title}>{group.title}</Text>
-        {links}
-      </Box>
-    );
-  });
-
   return (
-    <MotionDiv direction="up">
-      <footer className={classes.footer}>
-        <Container className={classes.inner} size="lg">
-          <Box className={classes.logo}>
-            <Image src="/logo.svg" className={classes.image} mb="sm" />
-            <Text size="xs" c="dimmed" className={classes.description}>
-              Build fully functional accessible web applications faster than ever
-            </Text>
-          </Box>
-          <Box className={classes.groups}>{groups}</Box>
-        </Container>
-        <Container className={classes.afterFooter} size="lg">
-          <Text c="dimmed" size="sm">
-            © 2020 mantine.dev. All rights reserved.
-          </Text>
-
-          <Group
-            gap={0}
-            className={classes.social}
-            justify="flex-end"
-            wrap="nowrap"
+    <footer className={classes.footer}>
+      <Container size="lg" className={classes.inner}>
+        <Link href="/" className={classes.logoText}>
+          <Text
+            size="xl"
+            fw={900}
+            variant="gradient"
+            gradient={{ from: "orange", to: "red", deg: 360 }}
           >
-            <ActionIcon size="lg" color="gray" variant="subtle">
-              <IconBrandTwitter
-                style={{ width: rem(18), height: rem(18) }}
-                stroke={1.5}
-              />
-            </ActionIcon>
-            <ActionIcon size="lg" color="gray" variant="subtle">
-              <IconBrandYoutube
-                style={{ width: rem(18), height: rem(18) }}
-                stroke={1.5}
-              />
-            </ActionIcon>
-            <ActionIcon size="lg" color="gray" variant="subtle">
-              <IconBrandInstagram
-                style={{ width: rem(18), height: rem(18) }}
-                stroke={1.5}
-              />
-            </ActionIcon>
-          </Group>
-        </Container>
-      </footer>
-    </MotionDiv>
+            ImAbhishek
+          </Text>
+        </Link>
+
+        <Title order={6} size={20} miw={250}>
+          Made with <TypingEffect />
+        </Title>
+
+        <Group gap="xs" justify="flex-end" wrap="nowrap">
+          <ActionIcon
+            size="lg"
+            radius="xl"
+            component="a"
+            href="https://iabhishek.tech"
+            target="_black"
+            variant="gradient"
+            gradient={{
+              from: "rgba(31, 30, 30, 1)",
+              to: "rgba(31, 31, 31, 1)",
+              deg: 0,
+            }}
+          >
+            <IconBrandGithub
+              style={{ width: rem(20), height: rem(20) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+          <ActionIcon
+            size="lg"
+            radius="xl"
+            component="a"
+            href="https://iabhishek.tech"
+            target="_black"
+            variant="gradient"
+            gradient={{ from: "rgba(0, 170, 255, 1)", to: "blue", deg: 270 }}
+          >
+            <IconBrandTwitter
+              style={{ width: rem(20), height: rem(20) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+          <ActionIcon
+            size="lg"
+            radius="xl"
+            component="a"
+            href="https://iabhishek.tech"
+            target="_black"
+            variant="gradient"
+            gradient={{ from: "red", to: "grape", deg: 227 }}
+          >
+            <IconBrandInstagram
+              style={{ width: rem(20), height: rem(20) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
   );
 }
