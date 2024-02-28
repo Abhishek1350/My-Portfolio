e7e1fc6a367570e4d27591cddb7cfcf9f973b0b3
Abhishek <abhibhardwaj1017@gmail.com>
2024-02-28 21:25:08 +0530

    Feat : Resend setup  for email sending

---
+++ app/contact/api/route.ts
@@ -0,0 +1,24 @@
+import { EmailTemplate } from '../../../components/email-template';
+import { Resend } from 'resend';
+import React from 'react';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: "Hello world",
+      react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+    });
+
+    if (error) {
+      return Response.json({ error });
+    }
+
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
