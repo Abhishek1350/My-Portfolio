1c7ff8b413f8ef476aed304705f9e463ea582542
Abhishek <abhibhardwaj1017@gmail.com>
2024-04-13 23:47:30 +0530

    Feat : Modern card design for testimonials

--- components/cards/testimonial-card.tsx
+++ components/cards/testimonial-card.tsx
@@ -1,46 +1,47 @@
 import { Avatar, Text, Paper, Box } from "@mantine/core";
 import { ITestimonial } from "@/sanity/lib/types";
 import { urlForImage } from "@/sanity/lib/image";
+import styles from "./testimonial-card.module.css"
 
 export function TestimonialCard({ item }: { item: ITestimonial }) {
   return (
     <Paper
-      radius="md"
       withBorder
       p="lg"
-      bg="var(--mantine-color-body)"
       mih={440}
-      mah={440}
+      className={styles.card}
     >
-      <Avatar
-        src={urlForImage(item.image.asset)}
-        size={120}
-        radius={120}
-        mx="auto"
-      />
-      <Text ta="center" fz="lg" fw={500} mt="md">
-        {item.title}
-      </Text>
-      <Box mt={5} mb={10}>
-        <Text c="dimmed" fz="sm" ta="center">
-          {item.position} <br/> @{" "}
-          <Text
-            ta="center"
-            c="dimmed"
-            fz="sm"
-            component="a"
-            href={item.companyUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            td="underline"
-          >
-            {item.companyName}
+      <Box className={styles.content}>
+        <Avatar
+          src={urlForImage(item.image.asset)}
+          size={120}
+          radius={120}
+          mx="auto"
+        />
+        <Text ta="center" fz="lg" fw={500} mt="md">
+          {item.title}
+        </Text>
+        <Box mt={5} mb={10}>
+          <Text c="dimmed" fz="sm" ta="center">
+            {item.position} <br /> @{" "}
+            <Text
+              ta="center"
+              c="dimmed"
+              fz="sm"
+              component="a"
+              href={item.companyUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              td="underline"
+            >
+              {item.companyName}
+            </Text>
           </Text>
+        </Box>
+        <Text fz="sm" ta="center" lineClamp={8}>
+          {item.description}
         </Text>
       </Box>
-      <Text fz="sm" ta="center" lineClamp={8}>
-        {item.description}
-      </Text>
     </Paper>
   );
 }
