6f1e894468d88934881263575d0fac844cd8d917
Abhishek <abhibhardwaj1017@gmail.com>
2024-04-14 20:12:31 +0530

    Fix : Email template

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -1,4 +1,4 @@
-import { Html, Heading, Text } from "@react-email/components";
+import { Html, Text } from "@react-email/components";
 
 interface EmailTemplateProps {
   name: string;
@@ -10,7 +10,6 @@ interface EmailTemplateProps {
 export function EmailTemplateForUser({ name, subject, message }: EmailTemplateProps) {
   return (
     <Html lang="en">
-      <Heading as="h1">Thank You for Contacting</Heading>
       <Text>Dear {name},</Text>
       <Text>I have received your email</Text>
       <Text>I appreciate your interest and will get back to you as soon as possible regarding your inquiry.</Text>
@@ -25,7 +24,7 @@ export function EmailTemplateForUser({ name, subject, message }: EmailTemplatePr
 export function EmailTemplateForAdmin({ name, email, subject, message }: EmailTemplateProps) {
   return (
     <Html lang="en">
-      <Heading as="h1">New Inquiry from portfolio</Heading>
+      <Text>Hey, you got a new message from your portfolio!</Text>
       <Text>Subject: {subject}</Text>
       <Text>Message: {message}</Text>
       <Text>From: {name}, ({email})</Text>
