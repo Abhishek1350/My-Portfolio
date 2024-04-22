6a8baca5472973b05f7b6dd5db827983ce674de9
Abhishek <abhibhardwaj1017@gmail.com>
2024-04-22 23:50:25 +0530

    UI Changes

--- components/forms/contact-form.tsx
+++ components/forms/contact-form.tsx
@@ -7,7 +7,6 @@ import {
     Box,
     Textarea,
     LoadingOverlay,
-    Loader,
     Dialog,
 } from "@mantine/core";
 import styles from "./contact-form.module.css";
@@ -130,7 +129,21 @@ export function ContactForm({ currentEmail }: { currentEmail: string }) {
             {!sent ? (
                 <Group justify="flex-end" mt="lg">
                     <button type="submit" className={styles.submitBtn}>
-                        {loading ? <Loader size={24} /> : "Submit"}
+                        <div className={styles.svgWrapper}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="24"
+                                height="24"
+                            >
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path
+                                    fill="currentColor"
+                                    d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                                ></path>
+                            </svg>
+                        </div>
+                        <span>Send</span>
                     </button>
                 </Group>
             ) : (
