cbfef3578294fc678adcdd311adc604fd74412f7
Abhishek <abhibhardwaj1017@gmail.com>
2025-08-15 19:46:55 +0530

    feat: Hero section

---
+++ src/config/data.ts
@@ -0,0 +1,41 @@
+export const socialLinks = [
+    {
+        id: 5,
+        priority: 1,
+        title: "GitHub",
+        url: "https://github.com/Abhishek1350",
+    },
+    {
+        id: 6,
+        priority: 2,
+        title: "LinkedIn",
+        url: "https://www.linkedin.com/in/abhishek-bhardwaj-76b9a721b/",
+    },
+    {
+        id: 7,
+        priority: 3,
+        title: "Twitter",
+        url: "https://twitter.com/Abhishek1350",
+    },
+    {
+        id: 8,
+        priority: 4,
+        title: "Instagram",
+        url: "https://www.instagram.com/imabhishek.dev/",
+    },
+];
+
+export const personalInfo = {
+    name: "Abhishek Bhardwaj",
+    title: "Software Developer | React, Next.js, Nest.js",
+    avatar: "/abhishek-bhardwaj.jpg",
+    email: "hello@imabhishek.site",
+    location: "Himachal Pradesh, India",
+    description:
+        "Self-taught Software Developer from Himachal Pradesh, India, crafting scalable and high-performance websites and applications. Skilled in React, Next.js, and NestJS, I started my journey during the lockdown and worked my way from complete beginner to building custom software solutions from scratch.",
+};
+
+export const heroSection = {
+    personalInfo: personalInfo,
+    socialLinks: socialLinks,
+};
