c2515b3ecd52ac2d59417c2703e1381190f90a9e
Abhishek <abhibhardwaj1017@gmail.com>
2025-08-15 22:11:01 +0530

    feat: Work Expereince

--- src/config/data.ts
+++ src/config/data.ts
@@ -39,3 +39,33 @@ export const heroSection = {
     personalInfo: personalInfo,
     socialLinks: socialLinks,
 };
+
+export const workExperience = [
+  {
+    company: "FAB Web Studio",
+    position: "ReactJS Developer",
+    period: "Jan 2024 - Present",
+    shortDesc: "Started as a ReactJS developer and gradually expanded into full-stack responsibilities, including backend development and AI integration. Working both independently and within teams to deliver scalable web applications. My full responsibilities include:",
+    bulletPoints: [
+      "Gathering client requirements and delivering tailored frontend and backend solutions",
+      "Integrating AI tools into internal projects to enhance automation and workflows",
+      "Building and deploying custom Shopify apps using the Remix framework",
+      "Developing responsive user interfaces with React.js, Next.js, and TypeScript",
+      "Integrating headless CMS platforms such as Strapi, Sanity, Drupal, and WordPress",
+      "Implementing backend systems using Node.js and NestJS with PostgreSQL",
+      "Managing deployments on VPS and optimizing performance with Cloudflare and GraphQL"
+    ]
+  },
+  {
+    company: "Edvolve",
+    position: "Software Engineer",
+    period: "Aug 2022 - Jan 2024",
+    shortDesc: "Joined as an intern and quickly transitioned into a full-time Software Engineer role. Worked closely with the founding team to build and launch key products from the ground up. My full responsibilities included:",
+    bulletPoints: [
+      "Built and launched two full-scale web applications using React",
+      "Worked directly with the founders to define product roadmap and strategy",
+      "Led frontend development and collaborated across teams in an agile environment",
+      "Contributed to funding pitches by preparing live product demos and prototypes"
+    ]
+  }
+];
