85aaab7218d0ccc73c18cc36f5edfcc01e482486
Abhishek <abhibhardwaj1017@gmail.com>
2025-10-10 22:33:26 +0530

    feat: video preload

--- src/components/ui/video-dialog.tsx
+++ src/components/ui/video-dialog.tsx
@@ -143,6 +143,7 @@ export function VideoDialog({
                                     playsInline
                                     controls
                                     muted
+                                    preload="auto"
                                 ></video>
                             </div>
                         </motion.div>
